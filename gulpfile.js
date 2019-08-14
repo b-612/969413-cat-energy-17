7ada3ffda89c201139d95404bebe1e972a198840
b-612 <fromtheorion@yandex.ru>
2019-08-14 21:29:20 +0300

    Добавил таск для отслеживания изменений в script.js

--- gulpfile.js
+++ gulpfile.js
@@ -114,6 +114,7 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("cssclean", "css"));
   gulp.watch("source/*.html", gulp.series("copyhtml")).on("change", server.reload);
   gulp.watch("source/img/svgsprite", gulp.series("svgsprite", "refresh"));
+  gulp.watch("source/js/script.js", gulp.series("jsmin", "refresh"));
 });
 
 gulp.task("build", gulp.series(
