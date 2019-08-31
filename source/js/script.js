ab2a26b55198726a86078e7aff51b9c1fd086aeb
b-612 <fromtheorion@yandex.ru>
2019-08-31 15:38:57 +0300

    Промежуточный коммит. Надо вернуть регулятор слайдов

--- source/js/script.js
+++ source/js/script.js
@@ -48,16 +48,16 @@ function sliderOn () {
   var buttonBefore = document.querySelector(".results-factoid__before-after-button--before");
   var buttonAfter = document.querySelector(".results-factoid__before-after-button--after");
   var toggle = document.querySelector(".results-factoid__toggle");
-  var slides = document.querySelector(".results-factoid__slides");
+  var slides = document.querySelector(".results-slider__slides");
 
   buttonBefore.onclick = slideBefore;
   buttonAfter.onclick = slideAfter;
 
   function slideBefore() {
-    if (slides.classList.contains("results-factoid__slides--after")) {
-      slides.classList.remove("results-factoid__slides--after");
+    if (slides.classList.contains("results-slider__slides--after")) {
+      slides.classList.remove("results-slider__slides--after");
     }
-    slides.classList.add("results-factoid__slides--before");
+    slides.classList.add("results-slider__slides--before");
     if (toggle.classList.contains("results-factoid__toggle--after")) {
       toggle.classList.remove("results-factoid__toggle--after");
     }
@@ -65,10 +65,10 @@ function sliderOn () {
   }
 
   function slideAfter() {
-    if (slides.classList.contains("results-factoid__slides--before")) {
-      slides.classList.remove("results-factoid__slides--before");
+    if (slides.classList.contains("results-slider__slides--before")) {
+      slides.classList.remove("results-slider__slides--before");
     }
-    slides.classList.add("results-factoid__slides--after");
+    slides.classList.add("results-slider__slides--after");
     if (toggle.classList.contains("results-factoid__toggle--before")) {
       toggle.classList.remove("results-factoid__toggle--before");
     }
